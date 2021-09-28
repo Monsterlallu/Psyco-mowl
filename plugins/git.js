c666346bcca15bc852424f9de965f1b19cd407c5
Monsterlallu <87799191+Monsterlallu@users.noreply.github.com>
2021-09-28 20:36:30 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater MONSTER*
 *owner number wa.me/917736129238*
 
 *Bot making video : https://youtu.be/fd0KPblop-k*
